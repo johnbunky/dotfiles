04aebbe9334279b1cd27c45b91c47b78a4a35266
johnbunky <johnbunky11@gmail.com>
2026-06-28 18:54:07 +0100

    hyprland setup: blur, waybar, powermenu, foot, kitty

--- scripts/powermenu.sh
+++ scripts/powermenu.sh
@@ -2,9 +2,9 @@
 OPTIONS="Logout\nReboot\nShutdown\nSuspend\nLock"
 CHOSEN=$(echo -e "$OPTIONS" | wofi --dmenu --prompt "Power")
 case $CHOSEN in
-  Logout)   swaymsg exit ;;
+  Logout)   hyprctl dispatch exit ;;
   Reboot)   systemctl reboot ;;
   Shutdown) systemctl poweroff ;;
   Suspend)  systemctl suspend ;;
-  Lock)     swaylock -f -c 000000 ;;
+  Lock)     hyprlock ;;
 esac
