e9149eb2df47c1291b75974fc2ab42c27624c24e
johnbunky <johnbunky11@gmail.com>
2026-06-30 09:01:54 +0100

    checkpoint before lua migration

---
+++ hypr/gestures.lua
@@ -0,0 +1,3 @@
+hl.gesture({ fingers = 3, direction = "horizontal", action = "workspace" })
+hl.gesture({ fingers = 3, direction = "up", action = "fullscreen" })
+hl.gesture({ fingers = 3, direction = "down", action = "close" })
